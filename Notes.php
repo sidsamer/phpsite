18e7329a515c4192cef7017be46a856b86174cb0
sidsamer <44743443+sidsamer@users.noreply.github.com>
2018-11-13 07:50:53 +0200

    Add files via upload

--- Notes.php
+++ Notes.php
@@ -4,19 +4,10 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+<script src="javascript.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="style.css">
 <head>
-<script>
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-</script>
 <style>
 .NoteWidth button{
 background-color:green;
@@ -37,13 +28,14 @@ border: 0px solid green;
 <body>
 <CENTER>
 <header><h1>Notes<h1></header>
-<div class="NoteForm">
+<div class="NoteForm";">
 <form action='Notes.php' method='post' >
 <input type="text" name="NoteName" placeholder="enter new/exist Notes name" required><br>
 <input type="text" name="Note" placeholder="Edit/Create Note" required><br>
 <button type="submit" value="Submit" name="submit">Create</button>
 </form>
 </div>
+
 <div class="NoteWidth">
 <ul>
 <?php
@@ -65,7 +57,7 @@ if(isset($_POST['submit']))
 	     while($row=mysqli_fetch_assoc($result))
 	   {
 		   $tmp="'".$row['title']."'";
-		   echo "<li>".$row['title'].':<br><button onclick="myFunction('.$tmp.');">+</button><br>'.
+		   echo "<li><p style='font-size:25px;'>".$row['title'].':</p><button onclick="NoteBody('.$tmp.');">+</button><br>'.
 		   '<div id='.$tmp.' style="display:none;">'.$row['body'].'</div></li>';
 	   }
 	   }
